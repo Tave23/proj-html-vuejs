fa460364b50d41e9c8b55ba08705b6c581a6cf51
Davide Tavella <tavella01@gmail.com>
2021-12-13 14:34:57 +0100

    bootstrap

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="header">
+     <div class="header-menu" >
+        <button type="button" class="btn btn-danger">Danger</button>
+     </div>
+  </div>
+</template>
+
+<script>
+export default {
+   name: 'Header',
+}
+</script>
+
+<style>
+
+#header{
+   height: 400px;
+   width: 100vw;
+   background-color: black;
+}
+
+.header-menu{
+   width: 100%;
+}
+
+</style>
