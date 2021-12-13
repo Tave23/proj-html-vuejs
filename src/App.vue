526cbcea56a28880f2f2f6b672adf0c249fa43b3
Davide Tavella <tavella01@gmail.com>
2021-12-13 14:34:49 +0100

    bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
 
 <script>
 
+import Header from './components/Header.vue';
+
 export default {
   name: 'App',
   components: {
-
+    Header
   }
 }
 </script>
 
 <style lang="scss">
+
+@import '~bootstrap';
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
 </style>
