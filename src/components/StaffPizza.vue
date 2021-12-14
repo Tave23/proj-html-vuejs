4b8164d6818cf2b8d0fc91ea5ce2f413e829f4e6
Davide Tavella <tavella01@gmail.com>
2021-12-14 15:11:34 +0100

    partners logo

--- src/components/StaffPizza.vue
+++ src/components/StaffPizza.vue
@@ -7,6 +7,17 @@
          <img src="../assets/img/h1-team-3a.jpg" alt="">
          <img src="../assets/img/h1-team-4a.jpg" alt="">
       </div>
+      
+      <!-- brand partner -->
+      <div class="partners">
+         <div class="logo-partners">
+            <img src="../assets/img/h1-clients-img-4.png" alt="">
+            <img src="../assets/img/h1-clients-img-3.png" alt="">
+            <img src="../assets/img/h1-clients-img-1.png" alt="">
+            <img src="../assets/img/h1-clients-img-2.png" alt="">
+            <img src="../assets/img/h1-clients-img-5.png" alt="">
+         </div>
+      </div>
    </div>
 </template>
 
@@ -24,14 +35,38 @@ export default {
 
 @import '../assets/style/vars.scss';
 
+.StaffPizza{
+   margin-bottom: 4px;
+}
 
 .img-staff{
    display: flex;
+   margin-bottom: 4px;
 }
 
 .img-staff img{
    width: 25%;
    height: auto;
+}
+
+.partners{
+   width: 100vw;
+   height: 250px;
+   background-color: $background-jumbo-testimonial;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+}
+
+.logo-partners{
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   width: 60%;
+}
+
+.logo-partners img{
+   width: calc(100% / 5);
 }
 
 </style>
